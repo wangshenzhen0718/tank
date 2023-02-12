655d953d3a2b09a6e4c411d828d901049b540b07
wangshenzhen0718 <57970628+wangshenzhen0718@users.noreply.github.com>
2023-02-12 22:51:08 +0800

    绘制坦克上右下左

---
+++ base/src/main/java/com/wang/tankgame04/Hero.java
@@ -0,0 +1,14 @@
+package com.wang.tankgame04;
+
+/**
+ * @Author:wsz
+ * @Date: 2023/2/12 15:30
+ * @Description:自己的tank
+ * @Version: 1.0
+ * @Since: 1.0
+ */
+public class Hero extends Tank {
+    public Hero(int x, int y) {
+        super(x, y);
+    }
+}
